495bcc94130a29e0b6c0c321da7a727e2c0b9fb1
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-21 04:58:13 -0300

    feat: add button component ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-import Image from "next/image";
+import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+import Link from "next/link";
 
 export default function Home() {
-    return <h1>asdadasd</h1>;
+    return (
+        <MaxWidthWrapper>
+            <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Your marketplace for high-quality{" "}
+                    <span className="text-blue-600">digital assets</span>.
+                </h1>
+                <p className="mt-6 text-lg max-w-prose text-muded-foreground">
+                    Welcome to DigitalHippo. Every asset on our plataform is
+                    verified by our team to ensure our highest quality
+                    standards.
+                </p>
+                <div className="flex flex-col sm:flex-row gap-4 mt-6">
+                    <Link href="/products" >Browse Trending</Link>
+                </div>
+            </div>
+        </MaxWidthWrapper>
+    );
 }
