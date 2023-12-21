45d704b6ad34f0296239a7d8720380be9507241c
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-21 06:26:40 -0300

    feat: create active link in navbar

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState } from "react";
+import { NavItem } from "./NavItem";
+// TODO Arrumar espaço entre o logo e os link 
+export function NavItems() {
+    const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+    const isAnyOpen = activeIndex !== null;
+
+    return (
+        <div className="flex gap-4 h-full">
+            {PRODUCT_CATEGORIES.map((category, i) => {
+                const handleOpen = () => {
+                    if (activeIndex === i) {
+                        setActiveIndex(null);
+                    } else {
+                        setActiveIndex(i);
+                    }
+                };
+
+                const isOpen = i === activeIndex;
+                return (
+                    <NavItem
+                        category={category}
+                        handleOpen={handleOpen}
+                        isOpen={isOpen}
+                        key={category.value}
+                        isAnyOpen={isAnyOpen}
+                    />
+                );
+            })}
+        </div>
+    );
+}
---
+++ src/config/index.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+type IProductCategories = {
+    label: string;
+    value: "ui_kits";
+    featured: {
+        name: string;
+        href: string;
+        imageSrc: string;
+    }
+};
+
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits" as const,
+        featured:  [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "/nav/ui-kits/mixed.jpg"
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg"
+            },
+            {
+                name: "Bestsellets",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg"
+            }
+        ]
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured:  [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg"
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg"
+            },
+            {
+                name: "Bestselling Icons",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg"
+            }
+        ]
+    }
+];
